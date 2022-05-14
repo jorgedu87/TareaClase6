da7342b6ae9ea11951b93ac1d6515e64f338b4ee
jorgedu87 <jorgedu87@outlook.com>
2022-05-14 15:21:54 -0300

    finalizado

--- TareaClase6/Program.cs
+++ TareaClase6/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1, 21);
+//Console.WriteLine($"el numero generado es {numeroSecreto}");
+
+
+int contador = 0;
+int numeroIngresado;
+do
+{
+    contador++;
+    Console.WriteLine("Ingrese un numero del 1 al 20");
+    numeroIngresado = int.Parse(Console.ReadLine());
+    if (numeroIngresado == numeroSecreto) { break; }
+
+    if (numeroIngresado > numeroSecreto)
+    { Console.WriteLine("el numero ingresado es mayor al numero secreto"); }
+    else
+    { Console.WriteLine("el numero ingresado es menor al numero secreto"); }
+
+}
+while (numeroIngresado != numeroSecreto);
+Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era: {numeroSecreto}. Lo has logrado en {contador} intentos");
+
+
